3c0180295d889fbf690da67569364a6275f3cd5d
Matthew <mbred002@odu.edu>
2021-10-29 23:26:41 -0400

    This is my final commit for the gradle assignment

--- src/test/java/VocabTest.java
+++ src/test/java/VocabTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat; 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matcher.*;
 
 /**
  * Test of the Vocabulary class
